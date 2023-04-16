5f2bdca619ddd69f51f4dc5e849658e8e7d2597b
Veronica45diaz <128390811+Veronica45diaz@users.noreply.github.com>
2023-04-16 16:07:55 -0300

    code js

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  const formData = new FormData(form);
+  const data = {};
+  for (let key of formData.keys()) {
+    data[key] = formData.get(key);
+  }
+  console.log(data);
+});*/
+
+const BtnEnviar = document.querySelector("#btnEnviar");
+BtnEnviar.addEventListener("click", function() {
+    let name = document.querySelector("#nombre").value;
+    let email = document.querySelector("#email").value;
+    alert("Nombre: " + name + " Email: " + email);
+
+    });
